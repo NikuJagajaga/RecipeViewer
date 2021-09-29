4c9d1742f9c810bc68dedde0fc2ebac72d61236c
DaniGoodwin <danmarkhunter13072006@gmail.com>
2021-09-29 23:22:02 +0300

    Now declarations are FULL!

--- RecipeViewer.d.ts
+++ RecipeViewer.d.ts
@@ -83,11 +83,89 @@ declare interface ItemList {
     getName(id: number, data?: number): string;
     setup(): void;
 }
+declare interface BehaviorTools {
+    readJson(path: string): any;
+    readListOfJson(path: string): any[];
+    getNumericID(key: string): number;
+    convertToItem(str: string): Tile;
+}
+declare interface BrewingRecipe {
+    recipeList: RecipePattern[];
+    registerRecipe(input: string, reagent: string, output: string): void;
+    recipeListOld: RecipePattern[];
+    getName(meta: number): string;
+}
+declare interface LikeFurnaceRecipe {
+    recipeList: RecipePattern[];
+    registerRecipe(input: string, output: string): void;
+    getAllList(): RecipePattern[];
+}
+declare interface StonecutterRecipe {
+    recipeList: RecipePattern[];
+    registerRecipe(input: { item: string, count?: number, data?: number }, output: { item: string, count?: number, data?: number }): void;
+}
+declare interface MainUI {
+    readonly slotCountX: 12;
+    page: number;
+    list: ItemInfo[];
+    liquidMode: boolean;
+    liqList: string[];
+    currentSortMode: number;
+    sortMode: { text: string, type: "id" | "name", reverse: boolean }[];
+    sortFunc: { id: (a: ItemInfo, b: ItemInfo) => number, name: (a: ItemInfo, b: ItemInfo) => 1 | -1 };
+    readonly slotCount: number;
+    readonly listWindow: { item: UI.Window, liquid: UI.Window };
+    readonly window: UI.WindowGroup;
+    readonly elements: java.util.HashMap<string, UI.Element>;
+    readonly elemSlots: UI.Element[];
+    readonly elemTanks: UI.Element[];
+    changeSortMode(notChange?: boolean): void;
+    updateWindow(): void;
+    openWindow(list?: ItemInfo[]): void;
+}
+declare interface RButton {
+    currentScreen: string;
+    data: {[screen: string]: string};
+    window: UI.Window;
+    putOnNativeGui(screenName: string, recipeTypeKey: string): void;
+    onNativeGuiChanged(screen: string): void;
+}
+declare interface ViewInfo {
+    mode: 0 | 1 | 2,
+    tray: string[],
+    id?: number,
+    data?: number,
+    liquid?: string,
+    isUsage?: boolean,
+    recipe?: string;
+}
+declare interface SubUI {
+    page: number;
+    list: RecipePattern[];
+    select: string;
+    cache: ViewInfo[];
+    readonly window: UI.WindowGroup;
+    setupWindow(): void;
+    getTarget(): ViewInfo;
+    openWindow(search: Tile | string, isUsage?: boolean): void;
+    updateWindow(): void;
+    changeWindow(index: number): void;
+    turnPage(page: number, force?: boolean): void;
+}
 declare interface RecipeViewerAPI {
     Core: RecipeViewerOld;
     ItemList: ItemList;
     RecipeType: typeof RecipeType;
     RecipeTypeRegistry: RecipeTypeRegistry;
+    requireGlobal(name: "BehaviorTools"): BehaviorTools;
+    requireGlobal(name: "BrewingRecipe"): BrewingRecipe;
+    requireGlobal(name: "BlastFurnaceRecipe"): LikeFurnaceRecipe;
+    requireGlobal(name: "SmokerRecipe"): LikeFurnaceRecipe;
+    requireGlobal(name: "CampfireRecipe"): LikeFurnaceRecipe;
+    requireGlobal(name: "StonecutterRecipe"): StonecutterRecipe;
+    requireGlobal(name: "MainUI"): MainUI;
+    requireGlobal(name: "RButton"): RButton;
+    requireGlobal(name: "SubUI"): SubUI;
 }
 declare namespace ModAPI {
     function addAPICallback(apiName: "RecipeViewer", func: (api: RecipeViewerAPI) => void): void;
